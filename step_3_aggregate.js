7148942414c4751f4b4f1778656d9ea8c7113295
Kevin Branigan <kevin@branigan.ca>
2023-06-11 21:56:22 -0400

    Well now I dunno what else to do

--- step_3_aggregate.js
+++ step_3_aggregate.js
@@ -6,6 +6,7 @@ const { allFiles, yards, vehicles, services, } = require("./vars");
 const outputFolder = 'output';
 
 const routes = {};
+const last = (arr) => arr[arr.length - 1];
 
 (async () => {
   for (var i = 0 ; i < allFiles.length ; i++) {
@@ -15,14 +16,16 @@ const routes = {};
     const data = new String(fs.readFileSync(filename));
     let lines = data.split('\n').map(line => line.split(','));
     const header = lines[0];
+
+    // console.log(header);asdf
     lines = lines.slice(1);
 
     for (var j = 0 ; j < lines.length ; j++) {
       const line = lines[j];
-      let [ fileName, pageName, routeName, branchName, serviceName, yardName, last_change, rt_distance ] = line;
+      let [ fileName, pageNum, routeName, branchName, serviceName, yardName, last_change, rt_distance ] = line;
 
-      // if (parseInt(routeName) != 6) continue; // kbfu
-      if (parseInt(routeName) > 10) continue; // kbfu
+      if (parseInt(routeName) != 9) continue; // kbfu
+      // if (parseInt(routeName) > 10) continue; // kbfu
 
       if (routes[routeName] === undefined) {
         routes[routeName] = {
@@ -43,6 +46,9 @@ const routes = {};
       if (branch.services[serviceName] === undefined) {
         branch.services[serviceName] = {
           numLines: 0,
+          changes: new Set(),
+          distances: new Set(),
+          subservices: [ {}, {}, {}, {}, {} ],
         };
       }
       const service = branch.services[serviceName];
@@ -50,11 +56,27 @@ const routes = {};
       route.numLines++;
       branch.numLines++;
       service.numLines++;
+      service.changes.add(last_change);
+      service.distances.add(rt_distance);
+
+      for (var column = 0 ; column < 5 ; column++) {
+        const row = line.slice(8 + column * 6, 8 + (column + 1) * 6);
+        const [ veh_type, num_veh, interval, run_time, term_time, avg_spd ] = row;
+        const subservice = { veh_type, num_veh, interval, run_time, term_time, avg_spd, rt_distance, };
+        const subserviceJSON = JSON.stringify(subservice);
+
+        if (service.subservices[column][subserviceJSON] == undefined) {
+          service.subservices[column][subserviceJSON] = { numLines: 0, rows: [] };
+        }
+
+        service.subservices[column][subserviceJSON].numLines++;
+        service.subservices[column][subserviceJSON].rows.push({ fileName, pageNum, last_change });
+      }
     }
   }
 
-  // console.log(JSON.stringify(routes, false, 4));
-  console.log(routes);
-  // console.log(Object.keys(routes).sort((a, b) => parseInt(a) - parseInt(b)).map(k => `${k}: ${routes[k]}`).join('\n'));
-
+  const log = ['9 BELLAMY', '9 Warden Stn-Scarborough Centre Stn', 'Saturday', '0', 1];
+  const firstSubServiceJSON = Object.keys(routes[log[0]].branches[log[1]].services[log[2]].subservices[log[3]])[log[4]];
+  console.log({loggin: log, firstSubServiceJSON});
+  console.log(routes[log[0]].branches[log[1]].services[log[2]].subservices[log[3]][firstSubServiceJSON]);
 })();
